dc06be29ef04aeed5ad0fdaf12d34497ed294c28
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2024-11-03 18:39:10 -0300

    ➕ Add i18next-parser to generate locales files

---
+++ src/locales/index.ts
@@ -0,0 +1,11 @@
+import en from "./resources/en/translation.json";
+import pt from "./resources/pt/translation.json";
+
+export const resources = {
+  en: {
+    translation: en,
+  },
+  pt: {
+    translation: pt,
+  },
+};
